a2c6b07d3580f6e5edfb2774a20f45ef44455208
stravkin <travkin@exportcenter.ru>
2024-02-21 10:35:36 +0300

    Исправлена модель данных

--- src/prod/main.py
+++ src/prod/main.py
@@ -9,9 +9,12 @@ createTable()  # создать таблицы в базе данных
 
 # Обновление справочников комтрейда
 list_manual = [
-    {'manual_id': 4, 'tabl_name': 'comtrade_partner'}, # справочник
+    {'manual_id': 4, 'tabl_name': 'comtrade_partner'},  # справочник
     {'manual_id': 5, 'tabl_name': 'comtrade_reporter'},  # справочник
     {'manual_id': 20, 'tabl_name': 'comtrade_cmd_h6'}  # справочник ТН ВЭД
 ]
 for manual in list_manual:
-    updateTablComtrade(manual['manual_id'], manual['tabl_name'])
+    try:
+        updateTablComtrade(manual['manual_id'], manual['tabl_name'])
+    except:
+        print('Ошибка загрузки справочника с Comtrade')
